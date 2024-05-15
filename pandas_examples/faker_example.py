0e89c7f3e503a713729a4608fe629655dbf94baa
Lina Blijleven <lina.blijleven@hu.nl>
2024-05-15 11:48:26 +0200

    Added an example of the faker library to generate some 'personal' data

---
+++ pandas_examples/faker_example.py
@@ -0,0 +1,37 @@
+"""
+build_dataframe.py
+
+Builds klantenbestand.csv for a dummy dataset.
+"""
+import pandas as pd
+from faker import Faker
+
+N = 100
+
+# Dummy info generator
+fake = Faker()
+
+
+def generate_fakes(n=N, f=fake.name) -> list:
+    """
+    Generate fake entries using a given function
+    :param n:   Number of fakes
+    :param f:   Function to generate fakes with
+    :return:    List of desired length
+    """
+    return [f() for _ in range(n-1)]
+
+
+# Aanmaken vanaf kolommen
+df = pd.DataFrame(
+    data={
+        'naam': generate_fakes(f=fake.name),
+        'land': generate_fakes(f=fake.country),
+        'addres': generate_fakes(f=fake.address),
+        'geboortedatum': generate_fakes(f=fake.date_of_birth)
+    },
+    index=generate_fakes(f=fake.email)
+)
+
+print(df.head())
+df.to_csv('faker.csv', index=False)
